6ca7dcd10c733d3cab7e2d568acaad3dc4af2f1e
pedrovotf <pedrovotf@gmail.com>
2025-06-08 12:59:06 -0300

    Up database signup and Down

---
+++ migration/database/postgres/auth/signup_DOWN.sql
@@ -0,0 +1,17 @@
+DROP TABLE user_details."user" CASCADE;
+
+DROP TABLE user_details."user_password" CASCADE;
+
+DROP TABLE user_details."user_password_history" CASCADE;
+
+DROP TABLE user_details."user_status" CASCADE;
+
+DROP TABLE journey."token_config" CASCADE;
+
+DROP TABLE journey."token_password" CASCADE;
+
+DROP SCHEMA user_details;
+
+DROP SCHEMA journey;
+
+DROP DATABASE signup;

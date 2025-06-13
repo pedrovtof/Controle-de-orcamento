ed8319374f439e0af090a2730085a7112fb2e465
pedrovotf <pedrovotf@gmail.com>
2025-06-13 07:11:55 -0300

    Add tailwind variables

--- code/frontend/easy-fin-by-nievo/src/pages/_templates/footer.js
+++ code/frontend/easy-fin-by-nievo/src/pages/_templates/footer.js
@@ -27,23 +27,37 @@ const menuData = [
     }
 ]
 
+const dic = { 
+    label: "CNPJ 00.000.000/0000-00",
+    value: "All Rights Reserved ©Pedrovotf"
+}
+
 function Footer(){
     return(
         <React.Fragment>
-            <div>
-                <div>
+            <div className='footer-container bg-gray-900 text-center  pb-4 rounded-t-lg'>
+                <div className='footer-list-div flex flex-col gap-2 p-8 items-center justify-around sm:flex-row sm:items-start sm:gap-6 sm:py-4'>
                     {
                         menuData.map((item, index)=>{
                             return(
                                 <React.Fragment key={`${item.menuTitle}-${index}`.trim()}>
-                                    <div>
-                                        <h1>{item.menuTitle}</h1>
-                                        <ul>
+                                    <div className={`footer-list-box-${index}`}>
+                                        <h1 className={`footer-list-box-title-${index} text-gray-500 hover:bg-gray-400 text-1xl underline`}>
+                                            {item.menuTitle}
+                                        </h1>
+                                        <ul className={`footer-list-box-ul-${index}`}>
                                             {
                                                 menuData[index].content.map((itemChild,indexChild)=>{
                                                     return(
                                                         <React.Fragment key={`${itemChild}-${indexChild}`.trim()}>
-                                                            <li>{itemChild}</li>
+                                                            <li className={`footer-list-ul-item-${index} p-1`}>
+                                                                <a 
+                                                                    href='https://www.linkedin.com/in/pedrovotf/'
+                                                                    target='blank'
+                                                                    className={`footer-list-ul-item-link-${index}  font-medium text-gray-500 hover:bg-gray-400`}>
+                                                                    {itemChild}
+                                                                </a>
+                                                            </li>
                                                         </React.Fragment>
                                                     )
                                                 })
@@ -55,8 +69,12 @@ function Footer(){
                         })
                     }
                 </div>
+                <div className='rights-reserved-span-div'>
+                    <span className='rights-reserved-span-value font-medium text-gray-500'>
+                        {dic.label} {dic.value}
+                    </span>
+                </div>
             </div>
-            <p>CNPJ 00.000.000/0000-00 All Rights Reserved ©Pedrovotf</p>
         </React.Fragment>
     )
 }

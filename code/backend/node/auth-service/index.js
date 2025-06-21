573ac65f4ce4966ae896d5f16f3cc71931feac47
pedrovotf <pedrovotf@gmail.com>
2025-06-21 12:35:51 -0300

    Add mensagem for notfound API auth-service

--- code/backend/node/auth-service/index.js
+++ code/backend/node/auth-service/index.js
@@ -4,7 +4,15 @@ app.use(express.json())
 const port = 8081
 
 app.get('/', (req, res) => {
-  res.json({message:'Hello World!'})
+  res.json({message:'Root route is empty'})
+})
+
+app.get('/test', (req,res)=>{
+  res.json({message:'Test route'})
+})
+
+app.use((req,res)=>{
+  res.status(404).json({message:'This route does not exist'})
 })
 
 app.listen(port, () => {
